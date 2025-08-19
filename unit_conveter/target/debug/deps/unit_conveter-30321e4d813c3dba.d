82b0771b003fafd6ccb4c8e112ba4b465d29233d
Clyn1 <ochiengclyntono@gmail.com>
2025-08-19 22:04:16 +0300

    unit_converter

---
+++ unit_conveter/target/debug/deps/unit_conveter-30321e4d813c3dba.d
@@ -0,0 +1,5 @@
+/home/clintonochieng/Desktop/Rust_projects/unit_conveter/target/debug/deps/unit_conveter-30321e4d813c3dba.d: src/main.rs
+
+/home/clintonochieng/Desktop/Rust_projects/unit_conveter/target/debug/deps/unit_conveter-30321e4d813c3dba: src/main.rs
+
+src/main.rs:
